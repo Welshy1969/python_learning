6aa0f1a5237753a5b414dffd7fe462e2a43849a9
Welshy1969 <paisou69@gmail.com>
2025-06-19 06:27:19 +0100

    Lotto_test.py

---
+++ test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import random
+from datetime import datetime
+from time import sleep
+
+now = datetime.now()
+
+epoch_time = now.timestamp()
+rseed = (epoch_time)
+random.seed(rseed)
+print ("Random Seed is epoch time: " + str(rseed))
+
+lotto_array = []
+num_games = input("How many games? : ")
+
+num_games = int(num_games)
+
+for x in range(num_games):
+    now = datetime.now()
+    epoch_time=now.timestamp()
+    random.seed=epoch_time
+    print(epoch_time)
+    for y in range(6):
+        ball_num = random.randint(1,45)
+        while ball_num in lotto_array:
+            ball_num = random.randint(1,45)
+        lotto_array.append(ball_num)
+
+    print ("Game " + str(x+1))
+    print (sorted(lotto_array)) ## Printed after sorting lowest to highest
+
+    sleep(1.75)
+
+    lotto_array = []
+
